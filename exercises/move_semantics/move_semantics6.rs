e43509d4328004c11691d7237567ed9ab8dc14a9
New0W <2856532040@qq.com>
2025-04-05 17:18:39 +0800

    update

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-
 fn main() {
     let data = "Rust is great!".to_string();
 
@@ -22,6 +21,7 @@ fn get_char(data: String) -> char {
 // Should take ownership
 fn string_uppercase(mut data: String) {
     data.to_uppercase();
+    //由于更改大小写时某些字符可以扩展为多个字符，因此此函数返回 String 而不是就地修改参数。
 
     println!("{}", data);
 }
